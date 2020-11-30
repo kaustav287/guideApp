28491caebbdb9aef89bbb53cbc2f07676f304a36
rev_matcher <gkaustav57@gmail.com>
2020-11-30 11:31:13 +0530

    set up dom elements and input objects + created intialisation function for App

--- core.js
+++ core.js
@@ -2,11 +2,44 @@ let UIelements = (function(){
 
     var tags = {
 
+        up : "up",
+        low : "low",
+        mid : "mid",
+        bottom : "bottom",
+        sidebar : "sidebar",
+        searchbar : "search",
+        navigation : "nav"
     }
-})
+
+    return {
+
+        get_input : function(){
+
+        },
+
+        get_tags : function(){
+
+            return tags
+        }
+    }
+})();
 
 
 
 let ControlModule = (function(ui){
 
+    var setupEvents = function(){
+
+     DOM =  UIelements.get_tags;
+     INPUT = UIelements.get_input();
+
+    }
+
+    return {
+
+        init : function(){
+            setupEvents();
+            console.log("Application is running");
+        }
+    }
 })(UIelements);
